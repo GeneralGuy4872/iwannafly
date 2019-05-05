f8472b0e60fbea5d62b072b7b7d6c7d0c614f7da
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-04 23:19:48 -0500

    Update shapes.h

--- models/shapes.h
+++ models/shapes.h
@@ -1,14 +1,16 @@
 //HERE BE DRAGONS
+//X⇗ Y→ Z↑ for now, will probably change
+//unroll the arrays so that each vec3 is on a seperate row while editing, but put them back where you found them when done
 
-hr2 = (float) root(2) / 2
+hr2 = (float) root(2) / 2 //Half Root 2
 
 #define bone_octo(R) {( (R,0,0) , (0,R,0) , (0,0,R) ),( (R,0,0) , (0,R,0) , (0,0,-R) ),( (R,0,0) , (0,-R,0) , (0,0,R) ),( (R,0,0) , (0,-R,0) , (0,0,-R) ),( (-R,0,0) , (0,R,0) , (0,0,R) ),( (-R,0,0) , (0,R,0) , (0,0,-R) ),( (-R,0,0) , (0,-R,0) , (0,0,R) ),( (-R,0,0) , (0,-R,0) , (0,0,-R) )}
 #define bone_tetra(H,W,D) {( (0,0,0) , (0,.5 * W,-H) , (0,.5 * -W,-H) ),( (0,0,0) , (0,.5 * W,-H) , (-D,0,0) ),( (0,0,0) , (0,.5 * -W,-H) , (-D,0,0) ),( (-D,0,0) , (0,.5 * W,-H) , (0,.5 * -W,-H) )}
 #define bone_inv_triangle(H,W) {( (0,0,0) , (0,.5 * W,-H) , (0,.5 * -W,-H) )}
-#define bone_rt_triangle(H,W) {( (0,0,0) , (-W,0,0) , (0,0,H) )}
-#define bone_bicone(L,R) {( (R,0,L/2) , (0,R,L/2) , (0,0,L) ),( (R,0,L/2) , (0,R,L/2) , (0,0,0) ),( (R,0,L/2) , (0,-R,L/2) , (0,0,L) ),( (R,0,L/2) , (0,-R,L/2) , (0,0,0) ),( (-R,0,L/2) , (0,R,L/2) , (0,0,L) ),( (-R,0,L/2) , (0,R,L/2) , (0,0,0) ),( (-R,0,L/2) , (0,-R,L/2) , (0,0,L) ),( (-R,0,L/2) , (0,-R,L/2) , (0,0,0) )}
-#define bone_pyra(H,B) {( (0,0,H) , (B * hr2,B * hr2,0) , (B * hr2,-B * hr2,0),( (0,0,H) , (B * hr2,B * hr2,0) , (-B * hr2,B * hr2,0),( (0,0,H) , (-B * hr2,-B * hr2,0) , (B * hr2,-B * hr2,0),( (0,0,H) , (-B * hr2,-B * hr2,0) , (-B * hr2,B * hr2,0),( (B * hr2,B * hr2,0) , (B * hr2,-B * hr2,0) , (-B * hr2,B * hr2,0),( (-B * hr2,-B * hr2,0) , (B * hr2,-B * hr2,0) , (-B * hr2,B * hr2,0)}
-#define bone_inv_pyra(H,B) {( (0,0,0) , (B * hr2,B * hr2,H) , (B * hr2,-B * hr2,H) ),( (0,0,0) , (B * hr2,B * hr2,H) , (-B * hr2,B * hr2,H) ),( (0,0,0) , (-B * hr2,-B * hr2,H) , (B * hr2,-B * hr2,H) ),( (0,0,0) , (-B * hr2,-B * hr2,H) , (-B * hr2,B * hr2,H) ),( (B * hr2,B * hr2,H) , (B * hr2,-B * hr2,H) , (-B * hr2,B * hr2,H) ),( (-B * hr2,-B * hr2,H) , (B * hr2,-B * hr2,H) , (-B * hr2,B * hr2,H)}
-#define bone_inv_rot_pyra(H,B) {( (0,0,0) , (B,0,H) , (0,B,H) ),( (0,0,0) , (B,0,H) , (0,-B,H) ),( (0,0,0) , (-B,0,H) , (0,B,H) ),( (0,0,0) , (-B,0,H) , (0,-B,H) ),( (B,0,H) , (0,B,H) , (0,-B,H) ),( (-B,0,H) , (0,B,H) , (0,-B,H) )}
-#define bone_haf_inv_rot_pyra(H,B) {( (0,0,0) , (-B,0,H) , (0,B,H) ),( (0,0,0) , (-B,0,H) , (0,-B,H) ),( (0,0,0) , (0,B,H) , (0,-B,H) ),( (-B,0,H) , (0,B,H) , (0,-B,H) )}
-#define bone_haf_rot_pyra(H,B) {( (0,0,H) , (-B,0,0) , (0,B,0) ),( (0,0,H) , (-B,0,0) , (0,-B,0) ),( (0,0,H) , (0,B,0) , (0,-B,0) ),( (-B,0,0) , (0,B,0) , (0,-B,0) )}
+#define bone_rt_triangle(L,H) {( (0,0,0) , (L,0,0) , (0,0,-H) )}
+#define bone_bicone(L,R) {( (L/2,0,R) , (L/2,R,0) , (L,0,0) ),( (L/2,0,R) , (L/2,R,0) , (0,0,0) ),( (L/2,0,R) , (L/2,-R,0) , (L,0,0) ),( (L/2,0,R) , (L/2,-R,0) , (0,0,0) ),( (L/2,0,-R) , (L/2,R,0) , (L,0,0) ),( (L/2,0,-R) , (L/2,R,0) , (0,0,0) ),( (L/2,0,-R) , (L/2,-R,0) , (L,0,0) ),( (L/2,0,-R) , (L/2,-R,0) , (0,0,0) )}
+#define bone_pyra(L,R) {( (L,0,0) , (0,R * hr2,R * hr2) , (0,R * hr2,-R * hr2) ),( (L,0,0) , (0,R * hr2,R * hr2) , (0,-R * hr2,R * hr2) ),( (L,0,0) , (0,-R * hr2,-R * hr2) , (0,R * hr2,-R * hr2) ),( (L,0,0) , (0,-R * hr2,-R * hr2) , (0,-R * hr2,R * hr2) ),( (0,R * hr2,R * hr2) , (0,R * hr2,-R * hr2) , (0,-R * hr2,R * hr2) ),( (0,-R * hr2,-R * hr2) , (0,R * hr2,-R * hr2) , (0,-R * hr2,R * hr2) )}
+#define bone_inv_pyra(L,R) {( (0,0,0) , (L,R * hr2,R * hr2) , (L,R * hr2,-R * hr2) ),( (0,0,0) , (L,R * hr2,R * hr2) , (L,-R * hr2,R * hr2) ),( (0,0,0) , (L,-R * hr2,-R * hr2) , (L,R * hr2,-R * hr2) ),( (0,0,0) , (L,-R * hr2,-R * hr2) , (L,-R * hr2,R * hr2) ),( (L,R * hr2,R * hr2) , (L,R * hr2,-R * hr2) , (L,-R * hr2,R * hr2) ),( (L,-R * hr2,-R * hr2) , (L,R * hr2,-R * hr2,H) , (L,-R * hr2,R * hr2)}
+#define bone_inv_rot_pyra(L,R) {( (0,0,0) , (L,0,R) , (L,R,0) ),( (0,0,0) , (L,0,R) , (L,-B,0) ),( (0,0,0) , (L,0,-R) , (L,B,0) ),( (0,0,0) , (L,0,-R) , (L,-B,0) ),( (L,0,R) , (L,R,0) , (L,-B,0) ),( (L,0,-R) , (L,R,0) , (L,-B,0) )}
+#define bone_haf_inv_rot_pyra(L,R) {( (0,0,0) , (L,0,-R) , (L,R,0) ),( (0,0,0) , (L,0,-R) , (L,-R,0) ),( (0,0,0) , (L,R,0) , (L,-R,0) ),( (L,0,-R) , (L,R,0) , (L,-R,0) )}
+#define bone_haf_rot_pyra(L,R) {( (L,0,0) , (0,0,-R) , (0,R,0) ),( (L,0,0) , (0,0,-R) , (0,-R,0) ),( (L,0,0) , (0,R,0) , (0,-R,0) ),( (0,0,-R) , (0,R,0) , (0,-R,0) )}
