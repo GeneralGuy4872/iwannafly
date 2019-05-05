6573fe437881900166bf234022a9f23d759d1455
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-05 04:31:26 -0500

    Update joystick.h

--- joystick.h
+++ joystick.h
@@ -1,2 +1,8 @@
-//ioctl interface for input via \dev\js0
+//ioctl interface for input via /dev/js0
 //you arrived here from main
+
+joyerr()
+  {
+  printf("/dev/js0: Not a typewriter\n");
+  abort();
+  }
