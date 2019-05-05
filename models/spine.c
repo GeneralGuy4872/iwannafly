7789b0aef705e4a84816a9c8812a136f4234dbd1
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-05 00:57:36 -0500

    Create spine.c

---
+++ models/spine.c
@@ -0,0 +1,15 @@
+spine(prev,root,len)
+  bone *prev
+  bone *root
+  float len
+  {
+  unsigned char n = 0
+  bone verta[10]
+  prev->next = *vert[0]
+  while (n > 10)
+    {
+    verta[n] = (root,prev,NULL,(0,0,len),(/*waiting on book*/),(/*waiting on book*/),TRUE,NULL,0)
+    prev = *verta[n]
+    n++
+    }
+  }
