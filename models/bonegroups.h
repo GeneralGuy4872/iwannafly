3db39b16d023614974da00484ed9f8ba76172ff7
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-06 04:04:52 -0500

    Update bonegroups.h

--- models/bonegroups.h
+++ models/bonegroups.h
@@ -12,7 +12,7 @@ spine(prev,root,len,nmax)
   bone verta[10]
   while (n < nmax)
     {
-    verta[n] = (root,prev,NULL,(1,1,1),(0,0,len),(0,0,0),(1,1,1),(0,0,0),(1,0,0,0,0,1,0,0,0,0,1,0,0,0,len,1),TRUE,NULL,0)
+    verta[n] = (root,prev,NULL,(1,1,1),(0,0,len),(0,0,0),(0,0,0),(1,0,0,0,0,1,0,0,0,0,1,0,0,0,len,1),TRUE,NULL,0)
     prev->next = *verta[n]
     prev = *verta[n]
     n++
@@ -30,7 +30,7 @@ phalanges(prev,root,len,nmax,thumb)
   bone phalng[nmax]
   while (n < nmax)
     {
-    phalng[n] = (root,prev,NULL,(0,1,n==0),(len,0,0),(0,0,0),(1,1,1),((thumb*M_PI)/2,0,0),(1,0,0,0,0,1,0,0,0,0,1,0,len,0,0,1),TRUE,NULL,0)
+    phalng[n] = (root,prev,NULL,(0,1,n==0),(len,0,0),(0,0,0),((thumb*M_PI)/2,0,0),(1,0,0,0,0,1,0,0,0,0,1,0,len,0,0,1),TRUE,NULL,0)
     prev->next = *phalng[n]
     prev = *phalng[n]
     len = len*invphi
@@ -51,7 +51,7 @@ limb2(prev,root,len,arm,Q,s)
   bone limb[nmax]
   while (n < 2)
     {
-    limb[n] = (root,prev,NULL,(arm||n==0,n==0,1),(len,0,0),(0,0,0),(1,1,1),(0,Q*s,0),(1,0,0,0,0,1,0,0,0,0,1,0,len,0,0,1),TRUE,NULL,0)
+    limb[n] = (root,prev,NULL,(arm||n==0,n==0,1),(len,0,0),(0,0,0),(0,Q*s,0),(1,0,0,0,0,1,0,0,0,0,1,0,len,0,0,1),TRUE,NULL,0)
     prev->next = *limb[n]
     prev = *limb[n]
     len = len/gratio
@@ -72,7 +72,7 @@ digiti(prev,root,len,Q,s)
   bone limb[nmax]
   while (n < 2)
     {
-    limb[n] = (root,prev,NULL,(n==0,n==0,1),(len,0,0),(0,0,0),(1,1,1),(0,Q*s*(.5^n==0),0),(1,0,0,0,0,1,0,0,0,0,1,0,len,0,0,1),TRUE,NULL,0)
+    limb[n] = (root,prev,NULL,(n==0,n==0,1),(len,0,0),(0,0,0),(0,Q*s*(.5^n==0),0),(1,0,0,0,0,1,0,0,0,0,1,0,len,0,0,1),TRUE,NULL,0)
     prev->next = *limb[n]
     prev = *limb[n]
     len = len*tovsil
@@ -93,7 +93,7 @@ avewing(prev,root,len,Q,s)
   bone limb[nmax]
   while (n < 2)
     {
-    limb[n] = (root,prev,NULL,(0,n==0,1),(len,0,0),(0,0,0),(1,s,1),(0,Q*(n!=0),0),(1,0,0,0,0,1,0,0,0,0,1,0,len,0,0,1),TRUE,NULL,0)
+    limb[n] = (root,prev,NULL,(0,n==0,1),(len,0,0),(0,0,0),(0,Q*(n!=0),0),(1,0,0,0,0,1,0,0,0,0,1,0,len,0,0,1),TRUE,NULL,0)
     prev->next = *limb[n]
     prev = *limb[n]
     len = len*gratio
