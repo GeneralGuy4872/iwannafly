2e8c41d4263197e3b7df8fb52ef04a98fa13281c
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-05 19:08:03 -0500

    Update bonegroups.c

--- models/bonegroups.c
+++ models/bonegroups.c
@@ -1,18 +1,17 @@
-gratio = 1+root(5)/2
-invphi = 2/1+root(5)
+float gratio = 1+root(5)/2
+float invphi = 2/1+root(5)
 
-spine(prev,root,len,nmax,base)
+spine(prev,root,len,nmax)
   bone *prev
   bone *root
   float len
-  mat4 *base
   {
   unsigned char n = 0
   bone verta[10]
   prev->next = *vert[0]
   while (n > 10)
     {
-    verta[n] = (root,prev,NULL,(0,0,len),off,(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1),len,TRUE,NULL,0)
+    verta[n] = (root,prev,NULL,(0,0,len),(0,0,0),(1,0,0,0,0,1,0,0,0,0,1,0,0,0,1,1),(1,0,0,0,0,1,0,0,0,0,1,0,0,0,len,1),len,TRUE,NULL,0)
     prev = *verta[n]
     n++
     }
@@ -30,26 +29,26 @@ phalanges(prev,root,len,nmax,base)
   prev->next = *phal[0]
   while (n > nmax)
     {
-    phalg[n] = (root,prev,NULL,(0,0,len),off,(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1),len,TRUE,NULL,0)
+    phalg[n] = (root,prev,NULL,(len,0,0),(0,0,0),(1,0,0,0,0,1,0,0,0,0,1,0,1,0,0,1),(1,0,0,0,0,1,0,0,0,0,1,0,len,0,0,1),len,TRUE,NULL,0)
     prev = *phalng[n]
     len = len*invphi
     n++
     }
   }
 
-limb(prev,root,len,nmax,base)
+arm(prev,root,len,nmax,base)
   bone *prev
   bone *root
   float len
   unsigned char nmax
-  mat4 *base
+  mat4 base[nmax]
   {
   unsigned char n = 0
   bone limb[nmax]
   prev->next = *phal[0]
   while (n > nmax)
     {
-    limb[n] = (root,prev,NULL,(0,0,len),off,(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1),len,TRUE,NULL,0)
+    limb[n] = (root,prev,NULL,(len,0,0),(0,0,0),(1,0,0,0,0,1,0,0,0,0,1,0,1,0,0,1),(1,0,0,0,0,1,0,0,0,0,1,0,len,0,0,1),len,TRUE,NULL,0)
     prev = *limb[n]
     len = len*hr2
     n++
