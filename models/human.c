b46d7889e1a804b35648fa0fcc9bf9ab0544e862
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-07 04:04:25 -0500

    Create human.c

---
+++ models/human.c
@@ -0,0 +1,16 @@
+skeleton *sculpt_human()
+  {
+  void *pointy
+  skeleton foo = (malloc(sizeof(bone)),!!!!!COMEBACKHERE!!!!!,((1,1,1),(0,0,0)))
+  *pointy = foo->root
+    {
+    polygon pelvis[1] = bone_inv_triangle(2,.5)
+    foo->root = (NULL,*pointy,NULL,(0,2,.5),(0,0,0),matgen_identity,matgen_translate(0,2,.5),FALSE,*pelvis,1)
+    //(*up,*prev,*next,(len.x,len.y,len.z),(trans.x,trans.y,trans.z),base,curr,drawline,*faces,n_polys)
+    }
+    {
+    bone *cervical = spine(foo->root,*pointy,.2,10)
+    *pointy = *cervical
+      {
+      bone *lshoulder = malloc(sizeof(bone))
+      *lshoulder = (*cervical,*pointy,NULL,(1,0,0),(0,0,0),matgen_z_deg(90,-1,1,-1),matgen_translate(1,0,0),TRUE,NULL,0)
