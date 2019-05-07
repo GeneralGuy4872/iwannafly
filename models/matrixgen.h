66b9d2a80f81b46b6eb263cfc77efb7f6e88d4cf
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-07 02:29:51 -0500

    Update matrixgen.h

--- models/matrixgen.h
+++ models/matrixgen.h
@@ -1,6 +1,6 @@
 //HERE BE DRAGONS
 
-#define matgen_master (R,P,A,X,Y,Z) (X*cos(A)*cos(P),Y*sin(A)*cos(P),Z*cos(A)*-sin(A),0, X*(-sin(A)*cos(R)+cos(A)*sin(P)*sin(R)),Y*(cos(A)*cos(R)+sin(A)*sin(P)*sin(R)),Z*(sin(A)*sin(P)*cos(R)+cos(P)*sin(R)),0, X*(sin(A)*sin(R)+cos(A)*sin(P)*cos(R)),Y*(cos(A)*-sin(R)+sin(A)*sin(P)*sin(R)),Z*(sin(A)*sin(P)*-sin(R)+cos(P)*cos(R)),0, 0,0,0,1)
+#define matgen_master (R,P,A,X,Y,Z) (X*cos(radians(mod(A,360)))*cos(radians(mod(P,360))),Y*sin(radians(mod(A,360)))*cos(radians(mod(P,360))),Z*cos(radians(mod(A,360)))*-sin(radians(mod(A,360))),0, X*(-sin(radians(mod(A,360)))*cos(radians(mod(R,360)))+cos(radians(mod(A,360)))*sin(radians(mod(P,360)))*sin(radians(mod(R,360)))),Y*(cos(radians(mod(A,360)))*cos(radians(mod(R,360)))+sin(radians(mod(A,360)))*sin(radians(mod(P,360)))*sin(radians(mod(R,360)))),Z*(sin(radians(mod(A,360)))*sin(radians(mod(P,360)))*cos(radians(mod(R,360)))+cos(radians(mod(P,360)))*sin(radians(mod(R,360)))),0, X*(sin(radians(mod(A,360)))*sin(radians(mod(R,360)))+cos(radians(mod(A,360)))*sin(radians(mod(P,360)))*cos(radians(mod(R,360)))),Y*(cos(radians(mod(A,360)))*-sin(radians(mod(R,360)))+sin(radians(mod(A,360)))*sin(radians(mod(P,360)))*sin(radians(mod(R,360)))),Z*(sin(radians(mod(A,360)))*sin(radians(mod(P,360)))*-sin(radians(mod(R,360)))+cos(radians(mod(P,360)))*cos(radians(mod(R,360)))),0, 0,0,0,1)
 #define matgen_scale (X,Y,Z) (X,0,0,0, 0,Y,0,0, 0,0,Z,0, 0,0,0,1)
 #define matgen_translate (X,Y,Z) (1,0,0,0, 0,1,0,0, 0,0,1,0, X,Y,Z,1)
 #define matgen_identity (1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1)
