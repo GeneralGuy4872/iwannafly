cb7aa179e2d4426260dbe5f50d327bcc28808dfd
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-10 23:26:17 -0500

    Update joystick.h

--- joystick.h
+++ joystick.h
@@ -1,6 +1,10 @@
 //ioctl interface for input via /dev/js0
 //you arrived here from main
 
+vec3 axis_buffer_0 = (0,0,0)
+vec3 axis_buffer_1 = (0,0,0)
+vec4 axis_buffer_2 = (0,0,0,0)
+
 joyerr()
   {
   printf(">>>ERROR!<<<\n~MEDITATION~\n\n/dev/js0: Not a typewriter\nexiting...\n");
