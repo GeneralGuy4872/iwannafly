88d52ed75cd42567686f789dbba3acc286434c5a
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-11 22:24:22 -0500

    Update matrixgen.h

--- models/matrixgen.h
+++ models/matrixgen.h
@@ -1,13 +1,13 @@
 //HERE BE DRAGONS
 
-#define M_TAU (2 * M_PI) //was worried about float errors, now too hard to remove the macro. It also declutters the math somewhat.
-#define matgen_master_deg (R,P,A,X,Y,Z) (X*cos(radians(mod(A,360)))*cos(radians(mod(P,360))),X*sin(radians(mod(A,360)))*cos(radians(mod(P,360))),X*cos(radians(mod(A,360)))*-1*sin(radians(mod(A,360))),0, Y*(-1*sin(radians(mod(A,360)))*cos(radians(mod(R,360)))+cos(radians(mod(A,360)))*sin(radians(mod(P,360)))*sin(radians(mod(R,360)))),Y*(cos(radians(mod(A,360)))*cos(radians(mod(R,360)))+sin(radians(mod(A,360)))*sin(radians(mod(P,360)))*sin(radians(mod(R,360)))),Y*(sin(radians(mod(A,360)))*sin(radians(mod(P,360)))*cos(radians(mod(R,360)))+cos(radians(mod(P,360)))*sin(radians(mod(R,360)))),0, Z*(sin(radians(mod(A,360)))*sin(radians(mod(R,360)))+cos(radians(mod(A,360)))*sin(radians(mod(P,360)))*cos(radians(mod(R,360)))),Z*(cos(radians(mod(A,360)))*-1*sin(radians(mod(R,360)))+sin(radians(mod(A,360)))*sin(radians(mod(P,360)))*sin(radians(mod(R,360)))),Z*(sin(radians(mod(A,360)))*sin(radians(mod(P,360)))*-1*sin(radians(mod(R,360)))+cos(radians(mod(P,360)))*cos(radians(mod(R,360)))),0, 0,0,0,1)
-#define matgen_master_rad (R,P,A,X,Y,Z) (X*cos(mod(A,M_TAU))*cos(mod(P,M_TAU)),X*sin(mod(A,M_TAU))*cos(mod(P,M_TAU)),X*cos(mod(A,M_TAU))*-1*sin(mod(A,M_TAU)),0, Y*(-1*sin(mod(A,M_TAU))*cos(mod(R,M_TAU))+cos(mod(A,M_TAU))*sin(mod(P,M_TAU))*sin(mod(R,M_TAU))),Y*(cos(mod(A,M_TAU))*cos(mod(R,M_TAU))+sin(mod(A,M_TAU))*sin(mod(P,M_TAU))*sin(mod(R,M_TAU))),Y*(sin(mod(A,M_TAU))*sin(mod(P,M_TAU))*cos(mod(R,M_TAU))+cos(mod(P,M_TAU))*sin(mod(R,M_TAU))),0, Z*(sin(mod(A,M_TAU))*sin(mod(R,M_TAU))+cos(mod(A,M_TAU))*sin(mod(P,M_TAU))*cos(mod(R,M_TAU))),Z*(cos(mod(A,M_TAU))*-1*sin(mod(R,M_TAU))+sin(mod(A,M_TAU))*sin(mod(P,M_TAU))*sin(mod(R,M_TAU))),Z*(sin(mod(A,M_TAU))*sin(mod(P,M_TAU))*-1*sin(mod(R,M_TAU))+cos(mod(P,M_TAU))*cos(mod(R,M_TAU))),0, 0,0,0,1)
-#define matgen_x_deg (R,X,Y,Z) (X,0,0,0, 0,Y*cos(radians(mod(R,360))),Y*sin(radians(mod(R,360))),0, 0,Z*-1*sin(radians(mod(R,360))),Z*cos(radians(mod(R,360))),0, 0,0,0,1)
-#define matgen_x_rad (R,X,Y,Z) (X,0,0,0, 0,Y*cos(mod(R,M_TAU)),Y*sin(mod(R,M_TAU)),0, 0,Z*-1*sin(mod(R,M_TAU)),Z*cos(mod(R,M_TAU)),0, 0,0,0,1)
-#define matgen_y_deg (P,X,Y,Z) (X*cos(radians(mod(P,360))),0,X*-1*sin(radians(mod(P,360))),0, 0,Y,0,0, Z*sin(radians(mod(P,360))),0,Z*cos(radians(mod(P,360))),0, 0,0,0,1)
-#define matgen_y_rad (P,X,Y,Z) (X*cos(mod(P,M_TAU)),0,X*-1*sin(mod(P,M_TAU)),0, 0,Y,0,0, Z*sin(mod(P,M_TAU)),0,Z*cos(mod(P,M_TAU)),0, 0,0,0,1)
-#define matgen_z_deg (A,X,Y,Z) (X*cos(radians(mod(A,360))),X*sin(radians(mod(A,360))),0,0, Y*-1*sin(radians(mod(A,360))),Y*cos(radians(mod(A,360))),0,0, 0,0,Z,0, 0,0,0,1)
-#define matgen_z_rad (A,X,Y,Z) (X*cos(mod(A,M_TAU)),X*sin(mod(A,M_TAU)),0,0, Y*-1*sin(mod(A,M_TAU)),Y*cos(mod(A,M_TAU)),0,0, 0,0,Z,0, 0,0,0,1)
-#define matgen_translate (L,W,H) (1,0,0,0, 0,1,0,0, 0,0,1,0, L,W,H,1)
+#define M_TAU (2 * M_PI)
+#define RAD (D) radians(D)
+#define matgen_master_deg (X,Y,Z) (cos(RAD(Z))*cos(RAD(Y)),sin(RAD(Z))*cos(RAD(Y)),cos(RAD(Z))*-1*sin(RAD(Z)),0, -1*sin(RAD(Z))*cos(RAD(X))+cos(RAD(Z))*sin(RAD(Y))*sin(RAD(X)),cos(RAD(Z))*cos(RAD(X))+sin(RAD(Z))*sin(RAD(Y))*sin(RAD(X)),sin(RAD(Z))*sin(RAD(Y))*cos(RAD(X))+cos(RAD(Y))*sin(RAD(X)),0, sin(RAD(Z))*sin(RAD(X))+cos(RAD(Z))*sin(RAD(Y))*cos(RAD(X)),cos(RAD(Z))*-1*sin(RAD(X))+sin(RAD(Z))*sin(RAD(Y))*sin(RAD(X)),sin(RAD(Z))*sin(RAD(Y))*-1*sin(RAD(X))+cos(RAD(Y))*cos(RAD(X)),0, 0,0,0,1)
+#define matgen_master_rad (X,Y,Z) (cos(Z)*cos(Y),sin(Z)*cos(Y),cos(Z)*-1*sin(Z),0, -1*sin(Z)*cos(X)+cos(Z)*sin(Y)*sin(X),cos(Z)*cos(X)+sin(Z)*sin(Y)*sin(X),sin(Z)*sin(Y)*cos(X)+cos(Y)*sin(X),0, sin(Z)*sin(X)+cos(Z)*sin(Y)*cos(X),cos(Z)*-1*sin(X)+sin(Z)*sin(Y)*sin(X),sin(Z)*sin(Y)*-1*sin(X)+cos(Y)*cos(X),0, 0,0,0,1)
+#define matgen_x_deg (X) (1,0,0,0, 0,cos(RAD(X)),sin(RAD(X)),0, 0,-1*sin(RAD(X)),cos(RAD(X)),0, 0,0,0,1)
+#define matgen_x_rad (X) (1,0,0,0, 0,cos(X),sin(X),0, 0,-1*sin(X),cos(X),0, 0,0,0,1)
+#define matgen_y_deg (Y) (cos(RAD(Y)),0,-1*sin(RAD(Y)),0, 0,1,0,0, sin(RAD(Y)),0,cos(RAD(Y)),0, 0,0,0,1)
+#define matgen_y_rad (Y) (cos(Y),0,-1*sin(Y),0, 0,1,0,0, sin(Y),0,cos(Y),0, 0,0,0,1)
+#define matgen_z_deg (Z) (cos(RAD(Z)),sin(RAD(Z)),0,0, -1*sin(RAD(Z)),cos(RAD(Z)),0,0, 0,0,1,0, 0,0,0,1)
+#define matgen_z_rad (Z) (cos(Z),sin(Z),0,0, -1*sin(Z),cos(Z),0,0, 0,0,1,0, 0,0,0,1)
 #define matgen_ident (1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1)
