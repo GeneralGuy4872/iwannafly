c72f96dd524308329fc49fddf267c062b77808e9
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-13 18:18:16 -0500

    Update main.c

--- main.c
+++ main.c
@@ -95,9 +95,9 @@ struct light
 	}
 //HERE BE DRAGONS
 #define SPEED (X) (X.wet ? (X.ground ? X.Spd.y : X.Spd.w) : (X.ground ? X.Spd.x : X.Spd.z))
-#define PHYSICS (X,Y,Z) ((X.Y + (Z * SPEED(X))) - MINMAG(X.Ff.x * SIGN(X.Y + (Z * SPEED(X) )),X.Y + (Z * SPEED(X)) ) / SANE((X.Ff.w * X.stat.y) + (X.Ff.y * not(X.stat.y))
-#define GRAVITY (X,Y,Z) (((X.Y + (Z * X.Spd.z)) - (grav * not(X.ground) * (X.bouyent * X.wet))) * not(X.ground)) / SANE((X.Ff.y * not(X.ground) * X.wet) + (X.Ff.z * not(X.ground) * not(X.wet)
-#define ROLL (X,Y,Z) ((X.Y + (Z * X.Spd.z)) * not(X.ground) / SANE((X.Ff.y * not(X.ground) * X.wet) + (X.Ff.z * not(X.ground) * not(X.wet)
+#define PHYSICS (X,Y,Z) ((X.Y + (Z * SPEED(X))) - MINMAG(X.Ff.x * SIGN(X.Y + (Z * SPEED(X) )),X.Y + (Z * SPEED(X)) ) / SANE((X.Ff.w * X.stat.y) + (X.Ff.y * !(X.stat.y))
+#define GRAVITY (X,Y,Z) (((X.Y + (Z * X.Spd.z)) - (grav * !(X.ground) * (X.bouyent * X.wet))) * !(X.ground)) / SANE((X.Ff.y * !(X.ground) * X.wet) + (X.Ff.z * !(X.ground) * !(X.wet)
+#define ROLL (X,Y,Z) ((X.Y + (Z * X.Spd.z)) * !(X.ground) / SANE((X.Ff.y * !(X.ground) * X.wet) + (X.Ff.z * !(X.ground) * !(X.wet)
 //end dragons
 
 void onstep_player ()
