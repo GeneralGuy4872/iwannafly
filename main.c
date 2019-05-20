e1d16b242825e749a95f1f9b1eb5fcfed0cbe87a
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-19 22:15:36 -0500

    Update main.c

--- main.c
+++ main.c
@@ -42,6 +42,7 @@
 //still might use them for pause menu
 
 #include "./main.h"
+#include "./timing.c"
 
 #include "./joystick.c"
 
@@ -63,11 +64,6 @@
 
 float grav = 9.8 / FPS //meters / second / microfortnight
 
-clock_t prevframe = clock()
-int msec
-int timer = 100
-bool pause FALSE
-
 struct entity
 	{
 	struct vector3 pos
@@ -75,7 +71,7 @@ struct entity
 	struct vector4 hitbox //x = radius, y = eye height offset, z = height, w = base bone z offset
 	struct vector3 Velo
 	struct vector3 Torq
-	float rot[4][4] = matgen_ident
+	matrix rot = matgen_ident
 	struct statreg stat //bool ground, bool wet, bool yinv, bool horiz, tern bouy, bool uv, bool infa NOT IMPLEMENTED
 	struct microvec collid //NOT IMPLEMENTED x = restrict x movement, y = restrict y movement, z = restrict z movement, w = paral/conf
 	struct vector3 Ff //x = Friction, y = Water Drag, z = Air Drag
@@ -88,22 +84,16 @@ struct entity
 	*along z at x = hitbox.radius otherwise. hitbox.w is always along z axis
 	*and should be equal to hitbox.x if collid.w == 1*/
 
-
-struct light
-	{
-	struct vector3 pos
-	struct vector3 size
-	struct truecolor emission
-	}
 //HERE BE DRAGONS
-#define SPEED (X) ((X.stat.wet ? (X.stat.ground ? X.Spd.y : X.Spd.w) : (X.stat.ground ? X.Spd.x : X.Spd.z)) * X.collid.w)
+#define SPEED(X) ((X.stat.wet ? (X.stat.ground ? X.Spd.y : X.Spd.w) : (X.stat.ground ? X.Spd.x : X.Spd.z)) * X.collid.w)
 #define INVPIT(X) (SANE(FSGN(X.stat.yinv)))
-#define PHYSICS (X,Y,Z) ((X.Y + (Z * SPEED(X))) - ( (MIN((X.Ff.x * X.stat.ground * (Z * SPEED(X))),fabs(X.Y + (Z * SPEED(X)))) * FSGN(X.Y + (Z * SPEED(X))) / SANE((X.Ff.w * X.stat.wet) + (X.Ff.y * !(X.stat.wet))
-#define GRAVITY (X,Y,Z) (((X.Y + (Z * X.Spd.z)) - (grav * !(X.stat.ground) * (X.stat.bouy * X.stat.wet))) * !(X.stat.ground)) / SANE((X.Ff.y * !(X.stat.ground) * X.stat.wet) + (X.Ff.z * !(X.stat.ground) * !(X.stat.wet))
-#define ROLL (X,Y,Z) ((X.Y + (Z * X.Spd.z)) * !(X.stat.ground) / SANE((X.Ff.y * !(X.stat.ground) * X.stat.wet) + (X.Ff.z * !(X.stat.ground) * !(X.stat.wet))
+#define PHYSICS(X,Y,Z) ((X.Y + (Z * SPEED(X))) - ( (MIN((X.Ff.x * X.stat.ground * (Z * SPEED(X))),fabs(X.Y + (Z * SPEED(X)))) * FSGN(X.Y + (Z * SPEED(X))) / SANE((X.Ff.w * X.stat.wet) + (X.Ff.y * !(X.stat.wet))
+#define GRAVITY(X,Y,Z) (((X.Y + (Z * X.Spd.z)) - (grav * !(X.stat.ground) * (X.stat.bouy * X.stat.wet))) * !(X.stat.ground)) / SANE((X.Ff.y * !(X.stat.ground) * X.stat.wet) + (X.Ff.z * !(X.stat.ground) * !(X.stat.wet))
+#define ROLL(X,Y,Z) ((X.Y + (Z * X.Spd.z)) * !(X.stat.ground) / SANE((X.Ff.y * !(X.stat.ground) * X.stat.wet) + (X.Ff.z * !(X.stat.ground) * !(X.stat.wet))
 //end dragons
 
-void onstep_player ()
+onstep_player (player)
+	struct entity player
 	{
 	player.Velo.x = PHYSICS(player,Velo.x,player_buffer.x0)
 	player.Velo.y = PHYSICS(player,Velo.y,player_buffer.y0)
@@ -114,11 +104,13 @@ void onstep_player ()
 	
 	player.pos.x = (player.pos.x + player.Velo.x)%360 //degrees
 	player.pos.y = (player.pos.y + player.Velo.y)%360 //degrees
-	//torii are easier than spheres. cylinders are a better approximation, but have invisible walls the entire length of the poles.
-	player.pos.z = MIN(MAX((player.pos.z + player.Velo.z),-128),1024) //meters, heightmap minimum;arbitrary ceiling
+	/*torii are easier than spheres. cylinders are a better approximation, but have invisible walls the entire length
+	* of the poles because their y axis is clamped. torri are modded instead of clamped*/
+	player.pos.z = CLAMP((player.pos.z + player.Velo.z),-128,1024) //meters, heightmap minimum;arbitrary ceiling
 	player.rot.x = (player.rot.x + player.Torq.x)%360 //degrees
-	player.rot.y = (player.rot.y + player.Torq.y)%360 //degrees
+	player.rot.y = CLAMP((player.rot.y + player.Torq.y),-90,90) //degrees
 	player.rot.z = (player.rot.z + player.Torq.z)%360 //degrees
+
 	if (player.stat.horiz)
 	{
 	matset_zeuler_deg(player.ori,player.rot.z,player.rot.y,player.rot.x,1,INVPIT(player),1)
@@ -126,50 +118,84 @@ void onstep_player ()
 	else
 	{
 	matset_master_deg(player.ori,player.rot.x,player.rot.y,player.rot.z,1,INVPIT(player),1)
-	}
+	}}
 
-struct cameratype camera
+struct cameratype
 	{
-	struct entity root
+	struct entity *root
 	struct vector4 coord
-	//camera origin = root.pos + {0,0,root.off.y}
+	matrix rot = matgen_ident
 	}
 
-void onstep_camera ()
+onstep_camera (camera)
+	struct cameratype camera
 	{
 	camera.coord.x = (camera.coord.x + camera_buffer.x)%360 //degrees
-	camera.coord.y = (camera.coord.y + camera_buffer.y)%360 //degrees
-	camera.coord.z = MIN(MAX((camera.coord.z + camera_buffer.z),-90),90) //meters, soccer pitch
-	camera.coord.w = MIN(MAX((camera.coord.w + camera_buffer.w),5),270) //degrees
+	camera.coord.y = (camera.coord.y + camera_buffer.y)%360 //degrees; no roll axis, so not clamped to 180
+	camera.coord.z = CLAMP((camera.coord.z + camera_buffer.z),-90,90) //meters, soccer pitch
+	camera.coord.w = CLAMP((camera.coord.w + camera_buffer.w),5,225) //degrees
+
+	matset_sphere_deg(camera,camera.coord.x,camera.coord.y,camera.coord.z,1,1)
 	}
 
-void onstep_buffers ()
-	{}
-
-void onstep_render ()
-	{}
-
-void onstep_master ()
+onstep_master ()
 	{
-	onstep_player()
-	//implement ai entitys
-	//implement collision checking
-	onstep_camera()
-	onstep_buffers()
-	onstep_render()
+	onstep_player(PLAYER)
+	onstep_camera(CAMERA)
+	input__onstep_buffers()
+	ready = TRUE
+	}
+
+onstep_paused ()
+	{
+	onstep_camera(CAMERA)
+	input__onstep_camonly()
+	ready = TRUE
 	}
 
 /*>>>MAIN<<<*/
-main () {
-	clock_t diff
-		while !pause {
-			diff = clock() - prevframe
-			msec = diff * 1000 / CLOCKS_PER_SEC
-			if (msec > timer) {
-				prevframe = clock
-				onstep_master()
+main ()
+	{
+	while run
+		{
+		if nextframe
+			{
+			if pause
+				{
+				if ready
+					{
+					glx__drawframe()
+					nextframe = FALSE
+					ready = FALSE
+					}
+				else
+					{
+					onstep_paused()
+					}
+			else
+				{
+				if ready
+					{
+					glx__drawframe()
+					nextframe = FALSE
+					ready = FALSE
+					}
+				else
+					{
+					onstep_master()
+					}
 				}
 			}
+		else
+			{}
+		}
+	printf("runlevel lowered, exiting...")
+	printf("DOTLOAD")
+	printf("DOTLOAD")
+	printf("DOTLOAD")
+	printf("\n")
+	exit(0)
+	}
 
 //reimpliment gimbal lock fixers
 
