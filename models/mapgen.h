37fcfc2a532e0e0a69798f784088336d52e60e3d
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-21 03:46:19 -0500

    Update mapgen.h

--- models/mapgen.h
+++ models/mapgen.h
@@ -1,4 +1,5 @@
-#define HMCOORD(X,Y,M) {X%360,Y%360,M[X][Y]}
+#define HMCOORD(M) M[xcoord%360][ycoord%360]
+#define HMVERT(M) {xcoord%360,ycoord%360,HMCOORD(M)}
 
 /*terrain generation should load smaller heightmaps that measure 60"*60"*256m (arcminutes,arcminutes,meters)
 * exactly 9 of these are loaded at all times based on player position on the torus.
@@ -8,9 +9,10 @@
 * the entire world map is stored in ram, as a pointer array would be larger than the
 * actual char array. (entire map is 127KiB, what's the point of splitting it?)*/
 
-struct polygon ground[7][7][2]
+struct vector3 ground[150]
 
-create_land()
+create_land(map)
+  struct torusmap map
   {
   unsigned char n = 0
   unsigned char m
@@ -24,8 +26,7 @@ create_land()
     ycoord = -4
     while (ycoord < ymax)
       {
-      ground[n][m][0] = {HMCOORD(xcoord,ycoord,map),HMCOORD((xcoord + 1),ycoord,map),HMCOORD(xcoord,(ycoord + 1),map)}
-      ground[n][m][1] = {HMCOORD((xcoord + 1),(ycoord + 1),map),HMCOORD((xcoord + 1),ycoord,map),HMCOORD(xcoord,(ycoord + 1),map)}
+      //line xy xy1;x1y;x1y1
       m++
       ycoord++
       }
@@ -48,11 +49,29 @@ refresh_land(player)
     ycoord = MYFLOOR(player.pos.x) - 4
     while (ycoord < ymax)
       {
-      ground[n][m][0] = {HMCOORD(xcoord,ycoord,map),HMCOORD((xcoord + 1),ycoord,map),HMCOORD(xcoord,(ycoord + 1),map)}
-      ground[n][m][1] = {HMCOORD((xcoord + 1),(ycoord + 1),map),HMCOORD((xcoord + 1),ycoord,map),HMCOORD(xcoord,(ycoord + 1),map)}
+      //line xy xy1;x1y;x1y1
       m++
       ycoord++
       }
     n++
     xcoord++
     }}
+
+/* ground collisions
+if x+y<1
+	if y<x
+		z = ((pos.x%1)*map[x][y]) + ((1 - pos.x%1)*map[x+1][y])/2
+	else if y>x
+		z = ((pos.y%1)*map[x][y]) + ((1 - pos.y%1)*map[x][y+1])/2
+	else
+		z = ((pos.x%1)*map[x][y]) + ((1 - pos.x%1)*((map[x+1][y]+map[x][y+1])/2))/2
+else if x+y>1
+	if y<x
+		z = ((pos.x%1)*map[x+1][y]) + ((1 - pos.x%1)*map[x+1][y+1])
+	else if y>x
+		z = ((pos.y%1)*map[x][y+1]) + ((1 - pos.y%1)*map[x+1][y+1])
+	else
+		z = ((pos.x%1)*((map[x+1][y]+map[x][y+1])/2))) + ((1 - pos.x%1)*map[x+1][y+1]/2
+else
+	z = ((pos.x%1)*map[x][y+1]) + ((1 - pos.x%1)*map[x+1][y])/2
+*/
