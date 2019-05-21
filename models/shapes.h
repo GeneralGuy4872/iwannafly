229701cff51ce82b642f5577c45a2ba322335855
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-21 01:21:36 -0500

    Update shapes.h

--- models/shapes.h
+++ models/shapes.h
@@ -4,12 +4,12 @@
 
 /*m_foo opcodes:
 *
-* 0 = line from the first entry to here; eats 1 entry
-* pos = wireframe of n consecutive lines; eats n+1 entrys
-* -1 = point; eats 1 entry
-* -2 = single line; eats 2 entrys
-* -3 = triangle; eats 3 entrys
-* -4 = quad; eats 4 entrys
+* -n = line from n nodes back to here; eats 1 entry
+* 0 = line from first entry to here; eats 1 entry
+* 1 = point; eats 1 entry
+* 2 = single line; eats 2 entrys
+* 3 = triangle; eats 3 entrys
+* 4 = quad; eats 4 entrys
 *
 * num_foo = {instructions,vectors}
 */
@@ -17,37 +17,37 @@
 #define v_line_x(L) {{0,0,0},{L,0,0}}
 #define v_line_y(L) {{0,0,0},{0,L,0}}
 #define v_line_z(L) {{0,0,0},{0,0,L}}
-#define m_line {-2}
+#define m_line {2}
 #define num_line {1,2}
 
 #define raw_wire_octo(A,N,E,S,W,Z) {Z,N,E,A,S,E,Z,S,W,A,N,W,Z}
 #define v_wire_octo(R) raw_wire_octo({0,0,R},{R,0,0},{0,R,0},{-R,0,0},{0,-R,0},{0,0,-R})
-#define m_wire_octo {12}
-#define num_wire_octo {1,13}
+#define m_wire_octo {2,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}
+#define num_wire_octo {12,13}
 
 #define raw_wire_crate(A,B,C,D,W,X,Y,Z) {A,B,X,A,W,X,Y,B,C,Y,Z,C,D,Z,W,D,A}
 #define v_wire_crate(X,Y,Z) raw_wire_crate({X/2,Y/2,Z},{X/2,-Y/2,Z},{-X/2,-Y/2,Z},{-X/2,Y/2,Z},{X/2,Y/2,0},{X/2,-Y/2,0},{-X/2,-Y/2,0},{-X/2,Y/2,0})
-#define m_wire_crate {16}
-#define num_wire_crate {1,17}
+#define m_wire_crate {2,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}
+#define num_wire_crate {16,17}
 
 #define v_poly_octo(R) {{{0,0,R},{R,0,0},{0,R,0}},{{0,0,R},{0,R,0},{-R,0,0}},{{0,0,R},{-R,0,0},{0,-R,0}},{{0,0,R},{0,-R,0},{R,0,0}},{{0,0,-R},{R,0,0},{0,-R,0}},{{0,0,-R},{0,-R,0},{-R,0,0}},{{0,0,-R},{-R,0,0},{0,R,0}},{{0,0,-R},{0,R,0},{R,0,0}}}
 #define v_poly_bicone(L,R) {{{L,0,0},{L/2,0,R},{L/2,R,0}},{{L,0,0},{L/2,R,0},{L/2,0,-R}},{{L,0,0},{L/2,0,-R},{L/2,-R,0}},{{L,0,0},{L/2,-R,0},{L/2,0,R}},{{0,0,0},{L/2,0,R},{L/2,-R,0}},{{0,0,0},{L/2,-R,0},{L/2,0,-R}},{{0,0,0},{L/2,0,-R},{L/2,R,0}},{{0,0,0},{L/2,R,0},{L/2,0,R}}}
-#define m_poly_octo {-3,-3,-3,-3,-3,-3,-3,-3}
+#define m_poly_octo {3,3,3,3,3,3,3,3}
 #define num_poly_octo {8,24}
 
 #define v_poly_low_half_oct(R,H) {{{R,0,0},{0,-R,0},{-R,0,0},{0,R,0}},{{0,0,-H},{R,0,0},{0,-R,0}},{{0,0,-H},{0,-R,0},{-R,0,0}},{{0,0,-H},{-R,0,0},{0,R,0}},{{0,0,-H},{0,R,0},{R,0,0}}}
-#define m_poly_pyra {-4,-3,-3,-3,-3}
+#define m_poly_pyra {4,3,3,3,3}
 #define num_poly_pyra {5,16}
 
 #define v_poly_tetra(H,X,Y) {{{0,0,0},{0,-Y/2,-H},{0,Y/2,-H}},{{0,0,0},{-X,0,0},{0,-Y/2,-H}},{{-X,0,0},{0,0,0},{0,Y/2,-H}},{{-X,0,0},{0,Y/2,-H},{0,-Y/2,-H}}}
-#define m_poly_tetra {-3,-3,-3,-3}
+#define m_poly_tetra {3,3,3,3}
 #define num_poly_tetra {4,12}
 
 #define v_poly_low_triangle(H,W) {{0,0,0},{0,W/2,-H},{0,-W/2,-H}}
 #define v_poly_triangle(H,W) {{0,-W/2,0},{0,0,H},{0,W/2,0}}
-#define m_triangle {-3}
+#define m_triangle {3}
 #define num_triangle {1,3}
 
 #define v_poly_cube(X,Y,Z) {{{X/2,Y/2,Z},{X/2,-Y/2,Z},{-X/2,-Y/2,Z},{-X/2,Y/2,Z}},{{X/2,-Y/2,Z},{X/2,Y/2,Z},{X/2,Y/2,0},{X/2,-Y/2,0}},{{X/2,Y/2,Z},{-X/2,Y/2,Z},{-X/2,Y/2,0},{X/2,Y/2,0}},{{-X/2,Y/2,Z},{-X/2,-Y/2,Z},{-X/2,-Y/2,0},{-X/2,Y/2,0}},{{-X/2,Y/2,Z},{X/2,Y/2,Z},{X/2,Y/2,0},{-X/2,Y/2,0}},{{X/2,-Y/2,0},{X/2,Y/2,0},{-X/2,Y/2,0},{-X/2,-Y/2,0}}}
-#define m_poly_cube {-4,-4,-4,-4,-4,-4}
+#define m_poly_cube {4,4,4,4,4,4}
 #define num_poly_cube {6,24}
