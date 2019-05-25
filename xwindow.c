e5c49c5f82a493d36f697055b9e0fe67ecb306cd
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-25 14:07:24 -0500

    Update xwindow.c

--- xwindow.c
+++ xwindow.c
@@ -1,5 +1,15 @@
 //you arrived here from main.c
 
+#include <X11/Intrinsic.h>
+#include <X11/StringDefs.h>
+#include <X11/CoreP.h>
+#include <X11/Xatom.h>
+#include <X11/Xaw/Form.h>
+#include <X11/Xaw/Box.h>
+#include <X11/Xaw/Label.h>
+#include <X11/Xaw/Command.h>
+#include <X11/Xaw/Toggle.h>
+
 #include "./xwindow.h"
 
 //see repo X11-Xt-Xaw-test
