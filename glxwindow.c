ff2eb22be5b3f2801e013062fb7cdcb3ea8a8e13
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-25 16:19:56 -0500

    Update glxwindow.c

--- glxwindow.c
+++ glxwindow.c
@@ -1,3 +1,55 @@
 //you arrived here from main.c
 
+#include <X11/Xlib.h>
+#include <X11/Xutils.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glx.h>
+
 #include "./glxwindow.h"
+
+GLXContext mainwindow = glXCreateNewContext(XOpenDisplay(NULL),);
+XStoreName(dpy, win, "Iwannafly PREALPHA MACRO_VERS Compiled on __DATE__");
+
+glx__SetCamera(camera)
+  struct cameratype camera
+  {    
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(camera.coord.w,1,.25,90);
+  }
+
+glx__DrawLoop(plugh)
+  struct world plugh
+  {
+  glClearColor(255,255,255,255);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  
+  SetCamera(plugh.cam)
+  
+  glMatrixMode(GL_MODELVIEW);
+  struct entity *nextent = plugh->ent
+  while (*nextent->next != NULL)
+  {
+  glLoadMatrix(plugh.rot);
+  unsigned char n = 0
+  struct bone *nextbone = plugh.dembones.root
+    {
+    glLoadMatrixf(plugh.rot)
+    glMultMatrixf(nextbone.base)
+    glGetfloatv(GL_MODELVIEW_MATRIX,nextbone.curr)
+    shapeloop(nextbone.shape.geom)
+    *nextbone = nextbone->next
+    n++
+    }
+  while (n < plugh.dembones.n_bones)
+    {
+    glLoadMatrixf(nextbone.up.curr)
+    gltranslate(bone.up.len.x * bone.off.x,bone.up.len.y * bone.off.y,bone.up.len.z * bone.off.z)
+    glMultMatrixf(nextbone.base)
+    glGetfloatv(GL_MODELVIEW_MATRIX,nextbone.curr) 
+    shapeloop(nextbone.shape.geom)
+    *nextbone = nextbone->next
+    n++
+    }
+  }
