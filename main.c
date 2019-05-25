88ab046d6cdf8908c80a370a7657743b9c7768c8
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-25 17:03:21 -0500

    Update main.c

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 *
 **************************************/
 
-#define MACRO_VERS 000000.000.01
+#define MACRO_VERS 000.000.00.01
 //NOTE: I've decided that it's faster to write first and worry about syntax later.
 //the resulting psudocode is largely C-based with elements borrowed from C++, GLSL, and Mathmatic Notation
 
