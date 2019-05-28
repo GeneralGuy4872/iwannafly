4c5dc5b2479f9aa0dfb241c5f43017ada17917b1
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-27 23:35:48 -0500

    Update mapgen.h

--- models/mapgen.h
+++ models/mapgen.h
@@ -5,7 +5,6 @@
 * exactly 9 of these are loaded at all times based on player position on the torus.
 * only the 200 vertexes surrounding the player will be loaded from their meshes; the ones in the set:
 * (floor(pos.x)-4 <= x <= ceil(pos.x)+4),(floor(pos.y)-4 <= y <= ceil(pos.y)+4)
-* vertecies that exit this range will be unloaded. every map has 36 sectors.
 * the entire world map is stored in ram, as a pointer array would be larger than the
 * actual char array. (entire map is 127KiB, what's the point of splitting it?)*/
 
