35bbd108adadbc4c1b8691a32e20aa43b6c1773e
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-28 00:10:01 -0500

    Update shapeloop.h

--- models/shapeloop.h
+++ models/shapeloop.h
@@ -78,26 +78,26 @@ struct shape subject
       }
     else if (mode == 6)
       {
-      glBegin(GL_TRIANGLE_FAN)
+      glBegin(GL_LINE_STRIP)
         while (iter && (n < subject.inum))
           {
           glvertex3f(subject.vertlist[subject.bytecode[n]].x,subject.vertlist[subject.bytecode[n]].y,subject.vertlist[subject.bytecode[n]].z)
           n++
           iter--
           }
-      glCullFace(GL_BACK)
       glEnd()
       mode = 0
       }
     else if (mode == 7)
       {
-      glBegin(GL_LINE_STRIP)
-        while (iter && n < 
+      glBegin(GL_TRIANGLE_FAN)
+        while (iter && n < subject.inum))
           {
           glvertex3f(subject.vertlist[subject.bytecode[n]].x,subject.vertlist[subject.bytecode[n]].y,subject.vertlist[subject.bytecode[n]].z)
           n++
           iter--
           }
+      glCullFace(GL_BACK)
       glEnd()
       mode = 0
       }
@@ -116,7 +116,7 @@ struct shape subject
     else if (mode == 9)
       {
       glBegin(GL_LINES)
-        while (iter && n < 
+        while (iter && n < subject.inum))
           {
           glvertex3f(subject.vertlist[0].x,subject.vertlist[0].y,subject.vertlist[0].z)
           glvertex3f(subject.vertlist[subject.bytecode[n]].x,subject.vertlist[subject.bytecode[n]].y,subject.vertlist[subject.bytecode[n]].z)
@@ -135,4 +135,24 @@ struct shape subject
           }
       glEnd()
       mode = 0
-      }
+<END>
+
+groundloop(subject)
+struct shape subject
+  {
+  unsigned char n = 0
+  unsigned char mode = 0
+  unsigned char iter = 0
+  glColor4b(subject.color.r,subject.color.g,subject.color.b,subject.color.a)
+  while (n < subject.inum)
+    {
+    glBegin(GL_TRIANGLE_FAN)
+      while (iter && n < subject.inum))
+        {
+        glvertex3f(subject.vertlist[subject.bytecode[n]].x,subject.vertlist[subject.bytecode[n]].y,subject.vertlist[subject.bytecode[n]].z)
+        n++
+        iter--
+        }
+    glEnd()
+    mode = 0
+<END>
