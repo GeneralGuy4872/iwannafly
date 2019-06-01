1957e02bc86e387496d29d23731d688fb0c9f2f8
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-31 22:40:37 -0500

    Update glxwindow.h

--- glxwindow.h
+++ glxwindow.h
@@ -22,5 +22,5 @@ glx__startup()
   glXMakeCurrent(monitor,WinAtt,glxc);
   glEnable(GL_CULL_FACE);
   glEnable(GL_DEPTH_TEST);
-  XStoreName(dpy, win, "Iwannafly PREALPHA MACRO_VERS Compiled on __DATE__");
+  XStoreName(dpy, win, VITALSTAT(REV_NOFORK));
   }
