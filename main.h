efce50bc21b39fa2f4e1350175a7990ae091a3f6
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-05-31 22:58:19 -0500

    Update main.h

--- main.h
+++ main.h
@@ -7,9 +7,9 @@
 * #define SHMEX(V,F) SHMLOCK(V); F; SHMUNLOCK(V);
 *
 * #define MMAKE(M,L,N) M = sizeof(L) mmem [N]
-* #define MMLOCK(V) some_func(V)
-* #define MMUNLOCK(V) sume_func(V)
-* #define MMEX(V,F) SHMLOCK(V); F; SHMUNLOCK(V);
+* #define MMLOCK(V) some_func(*V)
+* #define MMUNLOCK(V) sume_func(*V)
+* #define MMEX(V,F) MMLOCK(*V); F; MMUNLOCK(*V);
 */
 
 //these will eventualy be dependent on wait times, once I figure out how to fork without causing wabbits
