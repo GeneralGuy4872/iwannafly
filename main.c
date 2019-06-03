f939197e12c0a7b55b75e3ad2890ee2bbfef90d3
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-06-03 12:41:14 -0500

    Update main.c

--- main.c
+++ main.c
@@ -125,7 +125,7 @@ struct cameratype
 	struct entity *root
 	struct vector4 coord
 	matrix rot = matgen_ident
-	struct viewform format //0 = dec, 1 = bin, 2 = oct, 3 = hex : 0 = deg, 1 = rad, 2 = turn, 3 = gon : 0 = m/ or nmi/, 1 = km/ : 0 = /s, 1 = /h : infra : uv
+	struct viewform format //0 = dec, 1 = oct, -n = hex : 0 = deg, 1 = rad, 2 = turn, 3 = gon : 0 = m/ or nmi/, 1 = km/ : 0 = /s, 1 = /h : infra : uv
 	unsigned short points
 	//since degrees are technically base 60 and speed is a float, points is the only thing that uses base
 	}
