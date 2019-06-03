72edbd1c00348e6d1ff6f2256b81c28e9a82a81e
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-06-02 21:23:21 -0500

    Update init.c

--- init.c
+++ init.c
@@ -3,3 +3,109 @@
 #include "./init.h"
 
 #include "./config.c"
+
+#define BUFFER_MAX 512
+#define FORM_MAX 6
+
+struct smallsettings
+  {
+  unsigned char species : 3 // 0 = avian, 1 = human, 2 = merfolk, 3 = insectoid, 4 = feline, 5 = draconic
+  bool forcebten : 1 //0 = use base determined by species, 1 = self explanitory
+  bool yinv : 1 //0 = computer-style controls, 1 = airplane-style controls
+  unsigned char sealevel : 4
+  }
+
+bool run = 0
+struct settings iniparsed
+char joypath[16]
+char mappath[256]
+torusmap planet
+#define MAP planet
+entity player1
+#define PLAYER player1
+cameratype camera1
+#define CAMERA camera1
+world everything = {&MAP,&PLAYER,&PLAYER,NULL,NULL,&CAMERA}
+#define WORLD everything
+
+init__setup(argv)
+  int argc
+  char **argv
+  {
+  FILE ini_file
+  ini_file = fopen("../conf.ini","r")
+  char ini_next[BUFFER_MAX]
+  char *ini_data
+  char *ini_key
+  fread(ini_next,sizeof(char),sizeof(ini_next),inisettings)
+  fclose(ini_file)
+  ini_data = ini_file
+  ini_key = ini_file
+  while ( strtok(ini_file,"\n") != NULL )
+    {
+    if ( strtok(ini_data,"=") != NULL )
+      {
+      if !(strcmp("species",ini_key))
+        {
+        struct settings.species = atoi(ini_data)
+        }
+      else if !(strcmp("forcebten",ini_key))
+        {
+        struct settings.species = atoi(ini_data)
+        }
+      else if !(strcmp("sealevel",ini_key))
+        {
+        struct settings.sealevel = atoi(ini_data)
+        }
+      else if !(strcmp("joypath",ini_key))
+        {
+        strncpy(joypath,ini_data,16)
+        }
+      else if !(strcmp("mappath",ini_key))
+        {
+        strncpy(mappath,ini_data,256)
+        }
+      }
+    ini_data = ini_next
+    ini_key = ini_next
+    }
+  free(ini_next); free(ini_data); free(ini_key)
+    
+  if (settings.species < FORM_MAX)
+    {
+    if (settings.species == 0)
+      {
+      polymorph_avian(PLAYER)
+      }
+    else if (settings.species == 1)
+      {
+      polymorph_human(PLAYER)
+      }
+    else if (settings.species == 2)
+      {
+      polymorph_merfolk(PLAYER)
+      }
+    else if (settings.species == 3)
+      {
+      polymorph_insectoid(PLAYER)
+      }
+    else if (settings.species == 4)
+      {
+      polymorph_felid(PLAYER)
+      }
+    else if (settings.species == 5)
+      {
+      polymorph_draconic(PLAYER)
+      }
+    }
+  else
+    {
+    HARD_ERROR_MACRO("init","formtable","invalid value in species-field of ini file.")
+    }
+
+  FILE heightmap_file = fopen(mappath,"rb")
+  fread(MAP.dots,sizeof(char),sizeof(MAP.dots),heightmap_file)
+  fclose(heightmap_file)
+  
+  run = 1
+  }
