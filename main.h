356e05f8f86c590b1fcfbe962ad86b0f1b6abb52
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-06-03 15:03:36 -0500

    Update main.h

--- main.h
+++ main.h
@@ -368,6 +368,5 @@ bytevector4 radf_to_degbv(input)
 #define fprintdef(N,O) fprintf(O,"%3i*%2i'%2i\"%2i",N.w,N.z,N.y,N.x)
 #define printdeg(O) printf("%3i*%2i'%2i\"%2i",N.w,N.z,N.y,N.x)
 #define sprintpoints(C,O) (C.base == 0 ? sprintf("%05d",C.points) : (C.base > 0 ? sprintf("o%06o",C.points) : sprintf("$%02h",C.points)))
-#define DEG(R) 
 
 #define HARD_ERROR_MACRO(F,E,A) fprintf(stderr,"%s Fell at %s and couldn't get up\nadditional info: %s\n\nX_X rip\n",F,E,A);/*fakeinit__shutdown();*/ abort();
