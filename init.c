8060abd0beefd5929692ac03a7b10373c3f6406d
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-06-03 23:16:44 -0500

    Update init.c

--- init.c
+++ init.c
@@ -5,23 +5,17 @@
 #include "./config.c"
 
 #define BUFFER_MAX 512
-#define FORM_MAX 4
 
-struct smallsettings
-  {
-  unsigned char species : 3 // 0 = avian, 1 = human, 2 = merfolk, 3 = insectoid, 4 = feline, 5 = draconic
-  bool forcebten : 1 //0 = use base determined by species, 1 = self explanitory
-  bool yinv : 1 //0 = computer-style controls, 1 = airplane-style controls
-  bool prntlog : 1 //output a log file
-  char : 0
-  unsigned char sealevel : 8
-  }
-
+unsigned char sealevel
+bool forcebaseten
+bool yinv
+  
 bool run = 0
-struct settings iniparsed
 char joypath[16]
 char mappath[256]
 char logpath[64]
+bool dologs
+unsigned char base_species
 FILE logfile
 torusmap planet1
 entity player1
@@ -35,28 +29,24 @@ init__setup()
   {
   FILE ini_file
   ini_file = fopen("./conf.ini","r")
-  char ini_next[BUFFER_MAX]
-  char *ini_data
+  char ini_data[BUFFER_MAX]
   char *ini_key
-  fread(ini_next,sizeof(char),sizeof(ini_next),inisettings)
-  fclose(ini_file)
-  ini_data = ini_file
-  ini_key = ini_file
-  while ( strtok(ini_file,"\n") != NULL )
+  while fscanf(ini_file,"\n",ini_data)
     {
+    ini_key = ini_data
     if ( strtok(ini_data,"=") != NULL )
       {
       if !(strcmp("species",ini_key))
         {
-        struct settings.species = atoi(ini_data)
+        base_species = atoi(ini_data)
         }
-      else if !(strcmp("forcebten",ini_key))
+      else if !(strcmp("forcebaseten",ini_key))
         {
-        struct settings.species = atoi(ini_data)
+        forcebaseten = atoi(ini_data)
         }
       else if !(strcmp("sealevel",ini_key))
         {
-        struct settings.sealevel = atoi(ini_data)
+        sealevel = atoi(ini_data)
         }
       else if !(strcmp("joypath",ini_key))
         {
@@ -65,6 +55,10 @@ init__setup()
       else if !(strcmp("mappath",ini_key))
         {
         strncpy(mappath,ini_data,sizeof(mappath) - 1)
+        }
+      else if !(strcmp("dologs",ini_key))
+        {
+        dologs = atoi(ini_data)
         }
       else if !(strcmp("logpath",ini_key))
         {
@@ -75,62 +69,77 @@ init__setup()
         continue
         }
       }
-    ini_data = ini_next
-    ini_key = ini_next
     }
-  free(ini_next); free(ini_data); free(ini_key)
+  fclose(ini_file); free(ini_data); free(ini_key)
     
   switch (smallsettings.species)
     {
     case 0 :
       {
       polymorph_avian(PLAYER)
-      smallsettings.forcebten ? CAMERA.base = 0 : CAMERA.base = 1
+      forcebten ? CAMERA.base = 0 : CAMERA.base = 1
+      PLAYER.stat.yinv = yinv
       break
       }
     case 1 :
       {
       polymorph_human(PLAYER)
       CAMERA.base = 0
+      PLAYER.stat.yinv = yinv
+      break
       }
     case 2 :
       {
       polymorph_merfolk(PLAYER)
       CAMERA.base = 0
+      PLAYER.stat.yinv = yinv
+      break
       }
     case 3 :
       {
       polymorph_insectoid(PLAYER)
-      smallsettings.forcebten ? CAMERA.base = 0 : CAMERA.base = 1
+      forcebten ? CAMERA.base = 0 : CAMERA.base = 1
+      PLAYER.stat.yinv = yinv
       }/*
     case 4 :
       {
       polymorph_felid(PLAYER)
       CAMERA.base = 0
+      PLAYER.stat.yinv = yinv
+      break
       }
     case 5 :
       {
       polymorph_draconic(PLAYER)
       smallsettings.forcebten ? CAMERA.base = 0 : CAMERA.base = -1
+      PLAYER.stat.yinv = yinv
+      break
       }
     case 6 :
       {
       polymorph_elf(PLAYER)
       smallsettings.forcebten ? CAMERA.base = 0 : CAMERA.base = -1
+      PLAYER.stat.yinv = yinv
+      break
       }
     case 7 :
       {
       polymorph_dwarf(PLAYER)
       CAMERA.base = 0
+      PLAYER.stat.yinv = yinv
+      break
       }*/
     default :
       {
       SOFT_ERROR_MACRO("parseing of conf.ini by "__FILE__,"form switch default escape, at line#"__LINE__,"invalid value in species-field of ini file.")
       polymorph_avian(PLAYER)
-      smallsettings.forcebten ? CAMERA.base = 0 : CAMERA.base = 1
+      forcebten ? CAMERA.base = 0 : CAMERA.base = 1
+      PLAYER.stat.yinv = yinv
       break
       }
     }
+  
+  free(sealevel); free(forcebaseten); free(yinv);
 
   FILE heightmap_file = fopen(mappath,"rb")
   fread(MAP.dots,sizeof(char),sizeof(MAP.dots),heightmap_file)
@@ -138,7 +147,7 @@ init__setup()
   free(mappath)
   MAP.sealevel = smallsettings.sealevel
   
-  smallsettings.prntlog ? logfile = fopen(logpath,"w") : free(logfile)
+  dologs ? logfile = fopen(logpath,"w") : free(logfile)
   
   run = 1
   }
