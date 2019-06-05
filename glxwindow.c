b5a1593fb6fe3118e0baecf415aa0da4b14dfff6
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-06-05 17:51:57 -0500

    Update glxwindow.c

--- glxwindow.c
+++ glxwindow.c
@@ -19,39 +19,43 @@ glx__SetCamera(camera)
   gluPerspective(camera.coord.w,1,.25,100);
   }
 
-glx__DrawLoop(plugh)
-  struct world plugh
+glx__DrawLoop()
   {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
   glMatrixMode(GL_MODELVIEW);
-  struct entity *nextent = plugh->ent
-  while (*nextent->next != NULL)
-  {
-  glLoadMatrix(nextent.rot);
-  unsigned char n = 0
-  struct bone *nextbone = plugh.dembones.root
+  struct entity *nextent = WORLD->ent
+  while 1
     {
-    glLoadIdentity()
-    gltranslate(plugh.pos.x,plugh.pos.y,BASEBONEPOS(plugh))
-    glLoadMatrixf(plugh.rot)
-    glMultMatrixf(nextbone.base)
-    glGetfloatv(GL_MODELVIEW_MATRIX,nextbone.curr)
-    shapeloop(nextbone.shape.geom)
-    *nextbone = nextbone->next
-    n++
+    glLoadMatrix(nextent.rot);
+    struct bone *nextbone = nextent.dembones.root
+    for (n = 0,n < nextent.dembones.n_bones,n++)
+      {
+      if !n
+        {
+        glLoadIdentity()
+        gltranslate(nextent.pos.x,nextent.pos.y,BASEBONEPOS(nextent))
+        glMultMatrixf(plugh.rot)
+        }
+      else
+        {
+        glLoadMatrixf(nextbone.up.curr)
+        gltranslate(nextbone.up.len.x * FRFL(nextbone.off.x),nextbone.up.len.y * FRFL(nextbone.off.y),nextbone.up.len.z * FRFL(nextbone.off.z))
+        }
+      glMultMatrixf(nextbone.base)
+      glGetfloatv(GL_MODELVIEW_MATRIX,nextbone.curr) 
+      shapeloop(nextbone.shape)
+      }
+    glx__SetCamera(plugh.camera)
+    if (nextent->next != NULL)
+      {
+      nextent = nextent->next
+      }
+    else
+      {
+      return 0
+      }
     }
-  while (n < plugh.dembones.n_bones)
-    {
-    glLoadMatrixf(nextbone.up.curr)
-    gltranslate(bone.up.len.x * bone.off.x,bone.up.len.y * bone.off.y,bone.up.len.z * bone.off.z)
-    glMultMatrixf(nextbone.base)
-    glGetfloatv(GL_MODELVIEW_MATRIX,nextbone.curr) 
-    shapeloop(nextbone.shape.geom)
-    *nextbone = nextbone->next
-    n++
-    }
-  glx__SetCamera(plugh.camera)
   }
