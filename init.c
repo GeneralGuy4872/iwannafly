de7e88a372ffc8a2db2caf0d58366548614386c7
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-06-06 23:10:46 -0500

    Update init.c

--- init.c
+++ init.c
@@ -1,3 +1,4 @@
+//sed_flag_tabme
 //startup
 //you arived here from main
 #include "./init.h"
@@ -30,24 +31,24 @@ init__setup()
   FILE ini_file
   for(n = 1,n,n++)
     {
-    switch n
+    switch (n)
       {
-      case : 1
+      case 1 :
         {
         ini_file = fopen("~/.iwannafly/conf.ini","r")
         break
         }
-      case : 2
+      case 2 :
         {
         ini_file = fopen("/etc/iwannafly/conf.ini","r")
         break
         }
-      case : 3
+      case 3 :
         {
         ini_file = fopen("./conf.ini","r")
         break
         }
-      default
+      default :
         {
         HARD_ERROR_MACRO("init","line#"__LINE__", if-else failure escape","Cannot find conf.ini in (\"~/.iwannafly/\" | \"/etc/iwannafly/\" | \"./\") or access is denied")
         break
@@ -71,42 +72,42 @@ init__setup()
         {
         switch ini_key_hash
           {
-          case : HASH5('s','p','e') //the macro HASH5 it truncates 3 chars to the lower 5 bits, so is not case sensitive
+          case HASH5('s','p','e') ://the macro HASH5 it truncates 3 chars to the lower 5 bits, so is not case sensitive
             {
             base_species = atoi(ini_data)
             break
             }
-          case : HASH5('f','o','r')
+          case HASH5('f','o','r') :
             {
             forcebaseten = atoi(ini_data)
             break
             }
-          case : HASH5('s','e','a')
+          case HASH5('s','e','a') :
             {
             sealevel = atoi(ini_data)
             break
             }
-          case : HASH5('j','o','y')
+          case HASH5('j','o','y') :
             {
             strncpy(joypath,ini_data,sizeof(joypath) - 1)
             break
             }
-          case : HASH5('m','a','p')
+          case HASH5('m','a','p') :
             {
             strncpy(mappath,ini_data,sizeof(mappath) - 1)
             break
             }
-          case : HASH5('d','o','l')
+          case HASH5('d','o','l') :
             {
             dologs = atoi(ini_data)
             break
             }
-          case : HASH5('l','o','g')
+          case HASH5('l','o','g') :
             {
             strncpy(logpath,ini_data,sizeof(logpath) - 1)
             break
             }
-          default
+          default :
             {
             break
             }
@@ -195,31 +196,31 @@ init__setup()
 
   if dologs
     {
-    for (n = 1,n,n++)
+    for (n = 1;n;n++)
       {
-      switch n
+      switch (n)
         {
-        case : 1
+        case 1 :
           {
           logfile = fopen(logpath,"a")
           break
           }
-        case : 2
+        case 2 :
           {
           logfile = fopen("/var/log/iwannafly.log","a")
           break
           }
-        case : 3
+        case 3 :
           {
           logfile = fopen("~/.iwannafly.log","a")
           break
           }
-        case : 4
+        case 4 :
           {
           logfile = fopen("./iwannafly.log","a")
           break
           }
-        default
+        default :
           {
           SOFT_ERROR_MACR)("init","line#"__LINE__,"Cannot open a log file at specified path, or any of: \"/var/log/iwannafly.log\", \"~/.iwannafly.log\", \"./iwannafly.log\". What's up with your perms?!"}
           fclose(logfile)
