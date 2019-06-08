a881fb5e6ffe64327ad8d0ad0e808d0c283f73a5
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-06-08 06:38:53 -0500

    Update all.h

--- models/all.h
+++ models/all.h
@@ -14,39 +14,49 @@ polymorph_foo(entity *bar) changes an existing entity to the default values of f
 
 */
 
-polymorph_avian(target)
-  entity target
-  {
-  target.dembones = sculpt_avian()
-  target.hitbox = {AVE_RADIUS,AVE_HEIGHT,AVE_OFFSET}
-  target.stat.horiz = FALSE
-  target.stat.uv = TRUE
-  target.stat.infra = FALSE
-  target.Ff = {AVE_FRIC,AVE_DRAG_W,AVE_DRAG_Z}
-  target.Spd = {AVE_SPD_X,AVE_SPD_M,AVE_SPD_Z,AVE_SPD_W}
-  forcebaseten ? NOP : CAMERA.base = TRISTATE
-  }
+polymorph_avian(target,geomet)
+	entity target
+	mesurements geomet
+	{
+	target.dembones = sculpt_avian(geomet)
+	target.hitbox = {SUITFIT(geomet,hbradius),SUITFIT(geomet,hbheight),SUITFIT(geomet,hbeyes),SUITFIT(geomet,hboffset)}
+	target.stat.horiz = FALSE
+	target.stat.uv = TRUE
+	target.stat.infra = FALSE
+	target.Ff = {AVE_FRIC,AVE_DRAG_W,AVE_DRAG_Z}
+	target.Spd = {AVE_SPD_X,AVE_SPD_M,AVE_SPD_Z,AVE_SPD_W}
+	forcebaseten ? NOP : CAMERA.base = TRISTATE
+	}
 
-*entity summon_avian(xcoord,ycoord,zcoord)
-  float xcoord
-  float ycoord
-  float zcoord
-  {
-  struct entity tmp =
-	  {
-	  WORLD.ent_tail,
-	  NULL,
-	  {xcoord,ycoord,zcoord},
-	  {AVE_RADIUS,AVE_HEIGHT,AVE_OFFSET},
-	  {0,0,0},
-	  {0,0,0},
-	  matgen_ident,
-	  '\0',
-	  '\0',
-	  255,
-	  {AVE_FRIC,AVE_DRAG_W,AVE_DRAG_Z}
-	  {AVE_SPD_X,AVE_SPD_M,AVE_SPD_Z,AVE_SPD_W}
-	  sculpt_avian()
-    }
-  WORLD.ent_tail = &tmp
-  }
+*entity summon_avian(xcoord,ycoord,zcoord,geomet)
+	float xcoord
+	float ycoord
+	float zcoord
+	mesurements geomet
+	{
+	entity *parent
+	entity tmp = malloc(sizeof(struct entity))
+	if (WORLD.ent_tail != NULL)
+  		{
+		parent = WORLD.ent_tail
+		WORLD.ent_tail = &tmp
+		}
+	else
+		{
+		if (world.ent == NULL)
+			{
+			WORLD.ent = &tmp
+			WORLD.ent_tail = &tmp
+			}
+		else
+			{
+			WORLD.ent_tail = &tmp
+			parent = WORLD.ent
+			while (parent->next != NULL)
+				{
+				parent = parent->next
+				}
+			}
+  		}
+	struct entity tmp = {WORLD.ent_tail,NULL,{xcoord,ycoord,zcoord},{SUITFIT(geomet,hbradius),SUITFIT(geomet,hbheight),SUITFIT(geomet,hbeyes),SUITFIT(geomet,hboffset)},{0,0,0},{0,0,0},matgen_ident,'\0','\0',255,{AVE_FRIC,AVE_DRAG_W,AVE_DRAG_Z},{AVE_SPD_X,AVE_SPD_M,AVE_SPD_Z,AVE_SPD_W},sculpt_avian(geomet)}
+	}
