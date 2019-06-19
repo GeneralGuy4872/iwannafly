d57fd0b42ce4f5328ae04a3499e07f16c5c493c1
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-06-19 16:16:54 -0500

    Update all.h

--- models/all.h
+++ models/all.h
@@ -8,6 +8,11 @@
 /* *skeleton sculpt_foo(mesurements geomet) provided in each included file creates the model for an entity
  * (int) polymorph_foo(entity *bar,mesurements geomet) changes an existing entity to the default values of foo, keeping velocity, rotation, and position
  * (int) summon_foo(float xcoord,float ycoord,float zcoord,mesurements geomet) creates a new entity of the species foo and appends it to the list of entitys
+ *
+ * macros are for converting joystick inputs into speed and friction factors.
+ * these are currently chosen arbitrarily, but will eventually be based on classical physics
+ * (once I have a better understanding of the relevent equasions and coefficiants)
+ * so that they are consistant.
  */
 
 polymorph_avian(target,geomet)
