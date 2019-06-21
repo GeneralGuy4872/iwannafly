6d25ee56d2ac343ad7adf37d03084692150da73a
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-06-20 19:17:04 -0500

    Update joystick.h

--- joystick.h
+++ joystick.h
@@ -1,27 +1,40 @@
-//ioctl interface for input via /dev/js0
-//you arrived here from main
+struct js_event
+	{
+	unsigned int time;
+	signed short value;
+	unsigned char type;
+	unsigned char number;
+	}; //grumble grumble incomplete repos grumble
 
-struct inbuff
-  {
-  signed short x
-  signed short y
-  float yaw
-  float pit
-  float az
-  float alt
-  unsigned short lt
-  unsigned short rt
-  signed short zoom
-  unsigned short fov
-  }
-
-/*
-
-get js0
-pack js0 into joybuf
-get mouse xy
-pack mouse xy into moubuff.x/y
-on mouse scroll or page up/down, add/subtract from moubuff.z if within -90 <= z <= 90
-on left/right click, add/subtract from moubuff.w if doesn't overflow
-
-*/
+main()
+	{
+	volatile int joystick;
+	joystick = (open ("/dev/input/js0",O_RDONLY | O_NONBLOCK));
+	struct js_event event1;
+  signed short values[8]
+	while (1)
+		{
+		for(int n=0;n<SHRT_MAX;n++)
+			{
+			read(joystick,&event1,sizeof(event1));
+			if (event1.type == 0x02)
+				{
+				values[event1.number] = event1.value;
+				}
+			}
+    while (JSAXISFLAG < 0)
+      {
+      sleep(0)
+      }
+    JSAXISFLAG = 1
+    JSAXISBUFF[0] = values[0]
+    JSAXISBUFF[1] = values[1]
+    JSAXISBUFF[2] = values[2]
+    JSAXISBUFF[3] = values[3]
+    JSAXISBUFF[4] = values[4]
+    JSAXISBUFF[5] = values[5]
+    JSAXISBUFF[6] = values[6]
+    JSAXISBUFF[7] = values[7]
+    JSAXISFLAG = 0
+		}
+	}
