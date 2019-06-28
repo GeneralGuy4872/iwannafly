c200112914ff128bdd5b63ab4351e73403fd8d7f
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-06-28 08:16:24 -0500

    Update all.h

--- models/all.h
+++ models/all.h
@@ -6,7 +6,7 @@
 #include "draconic.c"
 #include "elf.c"
 
-/* *skeleton sculpt_foo(mesurements geomet) provided in each included file creates the model for an entity
+/* &bone sculpt_foo(mesurements geomet) provided in each included file creates the model for an entity
  * (int) polymorph_foo(entity *bar,mesurements geomet) changes an existing entity to the default values of foo, keeping velocity, rotation, and position
  * (int) summon_foo(float xcoord,float ycoord,float zcoord,mesurements geomet,bool yinv) creates a new entity of the species foo and appends it to the list of entitys
  *
