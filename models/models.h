cffc8fe466a8d3089f282724c782878bb6745514
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-06-28 08:15:06 -0500

    Update models.h

--- models/models.h
+++ models/models.h
@@ -1,55 +1,51 @@
 struct bone
   {
-  struct bone *up,
-  struct bone *prev,
-  struct bone *next,
-  struct charvector3 off,
-  struct vector3 len,
-  struct matrix base,
-  struct matrix curr,
-  struct truecolor color,
-  bool drawline,
-  struct shape *geom,
-  }
+  struct bone *up;
+  struct bone *prev;
+  struct bone *next;
+  struct charvector3 off;
+  struct vector3 len;
+  struct matrix base;
+  struct matrix curr;
+  struct truecolor color;
+  bool drawline;
+  struct shape *geom;
+  };
 
+/*
 struct aniframe
   {
-  struct aniframe *prev,
-  struct aniframe *next,
-  struct matrix *aniqueue[]
-  }
+  struct aniframe *prev;
+  struct aniframe *next;
+  struct matrix *aniqueue;
+  unsigned char n_frames;
+  };
 
 struct skeleton
   {
-  struct bone *root,
-  struct aniframe *aninext,
-  const unsigned char n_bones
-  }
+  struct bone *root;
+  struct aniframe *aninext;
+  const unsigned char n_bones;
+  };
 
 struct thing
   {
-  struct thing *prev,
-  struct thing *next,
-  const struct mat4 base,
-  struct mat4 curr,
-  struct shape geom
-  }
+  struct thing *prev;
+  struct thing *next;
+  const struct matrix base;
+  struct matrix curr;
+  struct shape *geom;
+  };
 
 animateskel(subject)
-  struct skeleton subject
+  struct skeleton subject;
   {
-  struct bone **current = subject.root
+  struct bone **current = subject.root;
   for (n = 0;n < subject.n_bones;n++)
     {
-    matpush(current.curr,subject.aninext.aniqueue[n])
-    current = current.next
+    matpush(current.curr,subject.aninext.aniqueue[n]);
+    current = current.next;
     }
-  subject.aninext = subject->aninext.next
+  subject.aninext = subject->aninext.next;
   }
-
-animatething(subject)
-  struct thing subject
-  {
-  matpush(subject.curr,aninext,aniqueue[0])
-  aninext = aninext->next
-  }
+*/
