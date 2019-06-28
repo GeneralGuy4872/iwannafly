0616802278790cdb5e9f0cfc33b1a61ef03c9cf8
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-06-28 04:02:24 -0500

    Update glxwindow.h

--- glxwindow.h
+++ glxwindow.h
@@ -4,78 +4,125 @@
 //need to nano -v headers and read docs/manpages to double-check stuff
 #define MAINWINSIZE 505
 
-Display *monitor = XOpenDisplay(NULL);
-Window desktop = DefaultRootWindow(monitor);
+Display *dsply = XOpenDisplay(NULL);
+Window desktop = DefaultRootWindow(dsply);
 int attrib[] = {GLX_RGBA,GLX_DEPTH_SIZE,24,GLX_DOUBLEBUFFER,None};
-XVisualInfo *VisInf = glXChooseVisual(monitor, 0, attrib);
-XSetWindowAttributes WinAtt;
-WinAtt.colormap = XCreateColormap(monitor,desktop,VisInf->visual,AllocNone);
-WinAtt.eventmask = ExposureMask | KeyPressMask | VisibilityChangeMask | Button1MotionMask | Button2MotionMask //start here and finnagle around with it if unexpected results
-Window mainwindow = XCreateWindow(monitor,desktop,0,0,MAINWINSIZE,MAINWINSIZE,0,VisInf->depth,InputOutput,VisInf->visual,CWColormap | CWEventMask,&swa);
-GLXContext glxc = glXCreateContext(monitor,VisInf,NULL,TRUE);
+XVisualInfo *VisInf = glXChooseVisual(dsply, 0, attrib);
+XSetWindowAttributes swa;
+Window glxwin = XCreateWindow(monitor,desktop,0,0,MAINWINSIZE,MAINWINSIZE,0,VisInf->depth,InputOutput,VisInf->visual,CWColormap | CWEventMask,&swa);
+swa.colormap = XCreateColormap(dsply,glxwin,VisInf->visual,AllocNone);
+swa.eventmask = ExposureMask | KeyPressMask | VisibilityChangeMask //start here and finnagle around with it if unexpected results
+GLXContext glxctx = glXCreateContext(monitor,VisInf,NULL,TRUE);
 XWindowAttributes GotWinAtt;
-XEvent x_event;
 
-glx__startup()
-  {
-  XMapWindow(monitor,mainwindow);
-  glXMakeCurrent(monitor,WinAtt,glxc);
-  glEnable(GL_CULL_FACE);
-  glEnable(GL_DEPTH_TEST);
-  XStoreName(dpy, win, VITALSTAT(REV_NOFORK));
-  }
-
-static void
-do_MotionNotify (XEvent *eventp)
+static unsigned int
+glx__keysymswitch(monitor,winder,event)
+	Display *monitor
+	Window winder
+	XEvent *event
 	{
-	XMotionEvent *e = (XMotionEvent *) eventp;
-
-	CAMBUFFER.az = e->x - (MAINWINSIZE / 2)
-	CAMBUFFER.alt = e->y - (MAINWINSIZE / 2)
-	}
-
-static void
-do_ButtonPress (XEvent *eventp)
-	{
-	XButtenEvent *e = (XButtonEvent *) eventp;
-
-	switch (e->button)
+	if (event->type == keypress)
 		{
-		case 4 :
+		sym_keypressd = XLookupKeysym(&event->xkey, 0);
+		switch (sym_keypressd)
 			{
-			CAMBUFFER.zoom++
-			break
-			}
-		case 5 :
-			{
-			CAMBUFFER.zoom--
-			break
-			}
-		case 6 :
-			{
-			CAMBUFFER.fov--
-			break
-			}
-		case 8 :
-			{
-			CAMBUFFER.fov--
-			break
-			}
-		case 7 :
-			{
-			CAMBUFFER.fov++
-			break
-			}
-		case 9 :
-			{
-			CAMBUFFER.fov++
-			break
-			}
-		default :
-			{
-			NOP
-			break
+			case XK_plus :
+				{
+				CAMBUFFER.w++
+				return STR_INT(0,'+',0,0)
+				break
+				}
+			case XK_minus :
+				{
+				CAMBUFFER.w--
+				return STR_INT(0,'-',0,0)
+				break
+				}
+			case XK_Home :
+				{
+				PLAYER.Torq.z = (PLAYER.torq.z + (CAMERA.coord.az * 2))%360
+				return STR_INT(0,'\r',0,0)
+				break
+				}
+			case XK_Left :
+				{
+				CAMBUFFER.x++
+				return STR_INT(0,0x1b,'[','D')
+				break
+				}
+			case XK_Right :
+				{
+				CAMBUFFER.x--
+				return STR_INT(0,0x1b,'[','C')
+				break
+				}
+			case XK_Up :
+				{
+				CAMBUFFER.y++
+				return STR_INT(0,0x1b,'[','A')
+				break
+				}
+			case XK_Down :
+				{
+				CAMBUFFER.y--
+				return STR_INT(0,0x1b,'[','B')
+				break
+				}
+			case XK_Page_Up :
+				{
+				CAMBUFFER.z++
+				return STR_INT(0,0x1b,'[','F')
+				break
+				}
+			case XK_Page_Down :
+				{
+				CAMBUFFER.z--
+				return STR_INT(0,0x1b,'[','E')
+				break
+				}
+			case XK_End :
+				{
+				CAMERA.coord.az=0
+				CAMERA.coord.alt=0
+				CAMERA.coord.z=0
+				CAMERA.coord.fov=50
+				return STR_INT(0,0x19,0,0)
+				break
+				}
+			default :
+				{
+				return -1
+				break
+				}
 			}
 		}
 	}
 
+static void
+glx__eventswitch(monitor,winder)
+	Display *monitor
+	Window winder
+	{
+	while TRUE
+		{
+		if XPending(monitor)
+			{
+			XEvent event
+			XNextEvent(monitor,&event)
+			glx__keysymswitch(monitor,winder,event)
+			}
+		}
+	}
+
+glx__startup()
+	{
+	XMapWindow(monitor,mainwindow);
+	glXMakeCurrent(dsply,glxwin,glxctx);
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_DEPTH_TEST);
+	XStoreName(dpy, win, VITALSTAT(REV_NOFORK));
+	glx__eventswitch(dsply,glxwin);
+	}
+	
+ * XK_End : camera.coord.az=0 camera.coord.alt=0 camera.coord.z=0 camera.coord.fov=50
+ */
