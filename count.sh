7f3ca63d5a62f8abad3974ef718f90fe7fa8bf1f
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2019-07-11 11:11:19 -0500

    Update count.sh

--- count.sh
+++ count.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 find  * -name "*.[ch]" | xargs wc -l > lines_of_c.log
 find  * -name "*.awk" -o -name "*.pl" -o -name "*.sh" -o -name "make" | xargs wc -l > lines_of_script.log
-find  * -name "*.xml" -o -name "*.json" -o -name "*.ini" -o -name "*.[tcd]sv" | xargs wc -l > lines_of_plugin.log
+find  * -name "*.xml" -o -name "*.json" -o -name "*.ini" -o -name "*.rc" -o -name ".*rc" -o -name "*.[tcd]sv" | xargs wc -l > lines_of_plugin.log
 find  * -name "*.hex" -o -name "*.bin" -o -name "*.dat" -o -name "*.raw" | xargs wc -c > bytes_of_binary.log
 find  * -name "*.txt" -o ! -name "*.*" -a ! -name "make" -a -type f| xargs wc -l > lines_of_text.log
 find  * -name "*.md" -o -name "*.html" -o -name "*.rtf" | xargs wc -l > lines_of_markup.log
