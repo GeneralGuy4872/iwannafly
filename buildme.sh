853ed9337124f7ead679ac9fc18b9606ffce4783
GeneralGuy4872 </dev/null>
2019-07-13 09:52:10 -0500

    push commit from localhost automate readme file

--- buildme.sh
+++ buildme.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
+cp conf.ini old_conf.ini
 cp defaults.ini conf.ini
-cc main.c -g -lX11 -lXaw -lXt -lm -lGL -lGLU
-date "+%H:%M:%S %a %b %d %Y" > date
-echo "update readme"
+touch date.tmp
+touch date
+if cc main.c -g -lX11 -lXaw -lXt -lm -lGL -lGLU; then
+printf "12s/.*/" > date.tmp;
+printf -- "- last segfault build : " >> date.tmp;
+date "+%H:%M:%S %a %b %d %Y %Z" >> date.tmp;
+else
+printf "14s/.*/" > date.tmp;
+printf -- "- last failed build : " >> date.tmp;
+date "+%H:%M:%S %a %b %d %Y" >> date.tmp;
+fi
+tr '\n' '/' <date.tmp >date
+printf "g" >> date
+rm date.tmp
+sed -i -f date README.md
+rm date
